570f59713ceb19fce6cbb57afbfba7aaf5f504d1
epdansereau <38821613+epdansereau@users.noreply.github.com>
2023-07-23 10:30:04 -0400

    Allow changing system during convo

--- conversation-flask.py
+++ conversation-flask.py
@@ -26,11 +26,24 @@ def start():
 @app.route('/message', methods=['POST'])
 def message():
     user_message = request.json['user_message']
+    system_prompt = request.json.get('system_prompt')  # Get system_prompt if it exists, else None
 
     if not llama_conversation.conversation:
         return jsonify({"error": "Please start a conversation first by calling /start"}), 400
 
+    # If a system prompt is provided
+    if system_prompt:
+        # Check if the first element in the conversation is a system prompt
+        if llama_conversation.conversation[0]['role'] == 'system':
+            # If so, update its content
+            llama_conversation.conversation[0]['content'] = system_prompt
+        else:
+            # If not, add a new system prompt to the start of the conversation
+            llama_conversation.conversation.insert(0, {'role': 'system', 'content': system_prompt})
+
     llama_conversation.conversation.append({'role':'user', 'content': user_message})
+
+    # Generate the prompt using the first message in the conversation as the system prompt
     prompt = llama_conversation.generate_prompt(llama_conversation.conversation, llama_conversation.conversation[0]['content'])
 
     output_text, assistant_message = llama_conversation.generate_response(prompt)
@@ -41,6 +54,7 @@ def message():
         "output_text": output_text,
         "conversation": llama_conversation.conversation
     }), 200
+
 
 if __name__ == "__main__":
     app.run(debug=False, port=5000)
